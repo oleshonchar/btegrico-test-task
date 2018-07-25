682096736adb81df0600452bef139391220495f7
oleshonchar <borovikoles@gmail.com>
2018-07-25 16:16:31 +0300

    Uploaded task#4

---
+++ task#4/custom_project_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Project Module',
+
+    'description': 'task#4',
+
+    'depends': ['base', 'project', 'project_team'],
+
+    'data': [
+        'security/project_security.xml',
+        'security/ir.model.access.csv',
+    ],
+}
+
